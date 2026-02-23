85feba1adfe860dad81ffe388e7d707078fd6545
MartAlx-RT <mav07@list.ru>
2026-02-23 23:48:28 +0300

    upd

--- test.asm
+++ test.asm
@@ -5,7 +5,6 @@ locals	@@
 VIDEOSEG	equ	0b800h
 
 .data
-	old_int		dd	19a400c1h
 .code
 
 org 100h
@@ -14,19 +13,16 @@ start	proc
 
 	mov	ax, 3509h
 	int	21h
-	mov	var1, bx
-	mov	var2, es
+	mov	old_adr, bx		; old interrupt's address
+	mov	old_seg, es
 
 	push	0
 	pop	es			; set es:[di] to int09 cell
 	mov	di, 4*09h
 
 	cli
-	push	es:[di]
-	push	es:[di+2]
-
 	mov	word ptr es:[di], offset @@handling
-	mov	word ptr es:[di+2], cs
+	mov	word ptr es:[di+2], cs	; write to the interrupt table
 	sti
 
 	mov	ax, 3100h
@@ -41,25 +37,27 @@ start	proc
 
 	push	VIDEOSEG
 	pop	es
-	mov	ah, 4eh			; pretty yellow on red style
-	mov	di, (80*5 + 40)*2
+	mov	ah, 4eh			; pretty yellow on red
+	mov	di, (80*5 + 40)*2	; at the center of the 5th line
 
 	in	al, 60h			; input scancode
 	mov	word ptr es:[di], ax	; draw it
 
 	in	al, 61h
 	or	al, 80h
-	out	61h, al			; confirm
+	out	61h, al			; confirm receipt
 	and	al, not 80h
 	out	61h, al
 
 	pop	es di ax
 
-	db	0eah
-	var1	dw	0
-	var2	dw	0
+;--------------------------call old interrupt---------------------------------
 
-EOP		db	0
+		db	0eah		; jmp far
+	old_adr	dw	0		;        :[old_adr]
+	old_seg	dw	0		; old_seg:
+
+	EOP		db	0	; end of program addr
 
 start	endp
 
