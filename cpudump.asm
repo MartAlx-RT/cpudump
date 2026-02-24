fa4092e39287964b7a2819511dad06787d6d0df2
MartAlx-RT <mav07@list.ru>
2026-02-24 21:27:30 +0300

    dump works

---
+++ cpudump.asm
@@ -0,0 +1,109 @@
+.286
+.model tiny
+
+locals	@@
+VIDEOSEG	equ	0b800h
+X_STARTPOS	equ	(80/2 - 9/2)*2
+START_POS	equ	(25/2 - 8/2)*80*2 + X_STARTPOS
+
+
+LINE_SIZE	equ	160		; line length in bytes (2 bytes/symbol)
+CENTER_POS	equ	80		; center of line in bytes
+VLINE		equ	0bah		; symbol that used as vertical line
+HLINE		equ	0cdh		; symbol that used as horizontal line
+LTOP		equ	0c9h		; left-top corner symbol
+RTOP		equ	0bbh		; right-top corner symbol
+LBTM		equ	0c8h		; left-bottom corner symbol
+RBTM		equ	0bch		; right-bottom corner symbol
+CLR_ATTR	equ	03h
+
+V_STARTPOS	equ	5d		; for box
+HOTKEY		equ	2		; key '1'
+
+.data
+	MSG_LEN		equ	5		; !!! HARDCODE !!! be careful
+	reg_msg		db	"AX = "
+			db	"CX = "
+			db	"DX = "
+			db	"BX = "
+			db	"SP = "
+			db	"BP = "
+			db	"SI = "
+			db	"DI = "
+			db	"DS = "
+			db	"ES = "
+	N_REGS		equ	($ - offset reg_msg)/MSG_LEN
+
+EOP	db	0		; end of program addr
+
+.code
+
+org 100h
+
+;------------------------------------------------------
+;-----------------ATTACH_INT FUNCTION------------------
+; set dumpbox interrupt
+;-------------------EXPECTED---------------------------
+; dx - number
+; di - end of string for writing to
+;-------------------RETURNS----------------------------
+; none
+;-------------------DESTROYS---------------------------
+; ax, cx and input parameters
+;------------------------------------------------------
+attach_int	proc
+
+	mov	ax, 3509h
+	int	21h
+	mov	old_adr, bx		; old interrupt's address
+	mov	old_seg, es
+
+	push	0
+	pop	es			; set es:[di] to int09 cell
+	mov	di, 4*09h
+
+	cli
+	mov	word ptr es:[di], offset @@handling
+	mov	word ptr es:[di+2], cs	; write to the interrupt table
+	sti
+
+	mov	ax, 3100h
+	mov	dx, offset EOP
+	shr	dx, 4
+	inc	dx
+	int	21h			; terminate & stay resident
+
+@@handling:
+	pusha
+	push	ds es
+
+	in	al, 60h			; input scancode
+;	mov	word ptr es:[di], ax	; draw it
+	cmp	al, HOTKEY
+	jne	@@old_int
+
+	call	dump
+
+	in	al, 61h
+	or	al, 80h
+	out	61h, al			; confirm receipt
+	and	al, not 80h
+	out	61h, al
+
+;----------------call old interrupt--------------------
+@@old_int:
+	pop	es ds
+	popa
+		db	0eah		; jmp far
+	old_adr	dw	0		;        :[old_adr]
+	old_seg	dw	0		; old_seg:
+
+attach_int	endp
+;------------------------------------------------------
+;------------------------------------------------------
+
+
+
+include	dumplib.asm
+
+end	attach_int
---
+++ dumplib.asm
@@ -0,0 +1,233 @@
+
+;------------------------------------------------------
+;-----------------PRINT_BOX  FUNCTION------------------
+; SETS ES:[DI] to (bl, bh) TO PRINT CENTERING
+;-------------------EXPECTED---------------------------
+; !! PASCAL CONVENTION !!
+; 1st arg(word) - box width
+; 2nd arg(word) - box height
+;-------------------RETURNS----------------------------
+; prints box with left-top at es:[di]
+;-------------------DESTROYS---------------------------
+; cx, ax and input parameters
+;------------------------------------------------------
+print_box proc
+	push bp
+	mov bp, sp
+
+	mov ah, CLR_ATTR
+
+	cmp word ptr [bp+6], 0
+		jz @@terminate_print_box			; if you wanna print empty box, go fuck yourself
+
+	xor cx, cx						; set counter (cx) to zero
+
+	cld								; forward 'stosw' mode
+
+	mov al, LTOP					; draw left-top corner
+	stosw
+
+	mov cx, [bp+6]
+	mov al, HLINE					; draw upper horizontal line
+	rep stosw
+
+	mov al, RTOP					; draw right-top corner
+	stosw
+	add di, LINE_SIZE-2				; -2 because 'stosw' adds 2 to di
+
+	mov cx, [bp+4]
+	mov al, VLINE
+	@@right:						; draw right line
+		stosw
+		add di, LINE_SIZE-2
+	loop @@right
+
+	std								; reverse 'stosw' mode
+
+	mov al, RBTM					; draw right-bottom corner
+	stosw
+
+	mov cx, [bp+6]
+	mov al, HLINE					; draw lower horizontal line
+	rep stosw
+	
+	mov al, LBTM					; draw left-bottom corner
+	stosw
+	sub di, LINE_SIZE-2
+
+	mov cx, [bp+4]
+	mov al, VLINE
+	@@left:							; draw left line
+		stosw
+		sub di, LINE_SIZE-2
+	loop @@left
+
+	@@terminate_print_box:
+	pop bp
+	ret 4
+print_box endp
+;------------------------------------------------------
+;------------------------------------------------------
+
+
+
+;------------------------------------------------------
+;-----------------CLEAR SHELL FUNCTION-----------------
+; clears the screen and set default video-mode (number 3)
+;-------------------EXPECTED---------------------------
+;-------------------RETURNS----------------------------
+; cleared screen =)
+;-------------------DESTROYS---------------------------
+; nothing
+;------------------------------------------------------
+cls proc
+	push ax cx es di
+
+	mov cx, 80*25
+	mov ax, VIDEOSEG
+	mov es, ax
+	xor di, di
+	xor ax, ax
+	mov ah, 07h
+	cld
+	rep stosw
+
+	pop di es cx ax
+	ret
+cls endp
+;------------------------------------------------------
+;------------------------------------------------------
+
+
+
+
+;------------------------------------------------------
+;-----------------STRNCPY       FUNCTION---------------
+; copy n bytes from 
+;-------------------EXPECTED---------------------------
+; cx - strlen
+; si - source string
+; es:[di] - address for copy to
+;-------------------RETURNS----------------------------
+; si/di - end of the copied/writed string
+;-------------------DESTROYS---------------------------
+; ax, cx, df
+;------------------------------------------------------
+strncpy proc
+	mov	ah, CLR_ATTR
+
+	test	cx, cx
+	jz	@@strncpy_exit			; length = 0 => exit
+
+	cld
+	@@cpy_loop:
+		lodsb
+		stosw						; copy to es:[di]
+	loop	@@cpy_loop
+
+	@@strncpy_exit:
+	ret
+strncpy endp
+
+
+
+
+
+;------------------------------------------------------
+;-----------------ITOA------FUNCTION-------------------
+; writes converted number (0..ffff) to hex as a string
+;-------------------EXPECTED---------------------------
+; dx - number
+; di - end of string for writing to
+;-------------------RETURNS----------------------------
+; none
+;-------------------DESTROYS---------------------------
+; ax, cx and input parameters
+;------------------------------------------------------
+itoa	proc
+	mov	cx, 4			; print 4 symbols
+	add	di, 6			; set di at the end of the line
+	mov	ah, CLR_ATTR
+
+	std
+@@itoa_loop:
+	mov	al, 0fh
+	and	al, dl			; al = lowest nibble
+
+	cmp	al, 0ah			; print digit or hex-letter
+	jl	@@is_digit
+	add	al, 'A' - '0' - 0ah
+@@is_digit:
+	add	al, '0'
+
+	stosw				; write to VRAM
+
+	shr	dx, 4			; next nibble
+loop	@@itoa_loop
+
+	ret
+itoa	endp
+;------------------------------------------------------------------------------------
+
+
+
+
+
+;------------------------------------------------------
+;-----------------DUMP      FUNCTION-------------------
+; dumps registers to the screen in pretty box
+;-------------------EXPECTED---------------------------
+; nothing
+;-------------------RETURNS----------------------------
+; none
+;-------------------DESTROYS---------------------------
+; nothing
+;------------------------------------------------------
+dump	proc
+
+	pusha					; push AX, CX, DX, BX, SP, BP, SI, DI
+	push	ds es				; save all registers
+
+	mov	bp, sp
+	add	bp, N_REGS*2			; set bp to ax (ss:[bp] == ax)
+
+	push	cs
+	pop	ds
+	mov	si, offset reg_msg		; set ds:[si] to reg_msg
+
+	push	VIDEOSEG
+	pop	es
+	mov	bx, V_STARTPOS * LINE_SIZE	; bx always points to beginning of the line
+
+@@dump_loop:
+	mov	di, bx
+	add	di, X_STARTPOS			; set di to new line
+	mov	cx, MSG_LEN
+	call	strncpy				; si -> new reg msg, di -> end of printing
+
+	add	bx, LINE_SIZE
+
+	sub	bp, 2
+	mov	dx, ss:[bp]			; get register value
+	call	itoa
+
+	cmp	bp, sp
+jne	@@dump_loop
+
+	mov	di, (V_STARTPOS-1) * LINE_SIZE
+	add	di, X_STARTPOS - 2		; set es:[di] to print box
+	push	(MSG_LEN+4)
+	push	N_REGS
+	call	print_box			; print box
+
+	pop	es ds
+	popa					; restore all regs
+
+;	int	20h
+	ret
+
+dump	endp
+;------------------------------------------------------
+;------------------------------------------------------
+
+;------------------------------------------------------------------------------------
